73278b0ee977b0dd10504e46a20eb3fc565dc3f9
Sofia Shulyak <s.shulyak@innopolis.university>
2024-10-01 21:37:02 +0300

    added streamlit app

---
+++ code/deployment/app/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import requests
+
+st.write("Identify the category of goods by a review")
+
+# Create text input fields
+title = st.text_input("Title")
+text = st.text_input("Text")
+
+# Create a submit button
+if st.button("Submit"):
+    # Make a GET request with the text as parameters
+    response = requests.get(f"http://localhost:8000/predict/title={title}&text={text}")
+
+    # Display the response text
+    st.write("Predicted category: "+response.json()['result'])
